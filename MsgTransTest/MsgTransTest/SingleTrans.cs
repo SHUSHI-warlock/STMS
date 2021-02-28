dc2cce24bb385499e36e0b8bec2cde9ff4bace9c
Kvalwalls <1970587024@qq.com>
2021-02-28 22:41:28 +0800

    SingleTrans

---
+++ MsgTransTest/MsgTransTest/SingleTrans.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Sockets;
+
+namespace MsgTransTest
+{
+    //统一使用MsgSendReceiver
+    //通过调用GetTransXXX进行传输
+    class SingleTrans
+    {
+        private readonly MsgSendReceiver msgSendReceiver;
+        private readonly TransDGL transDGL;
+        private readonly TransDKJ transDKJ;
+        private readonly TransKGL transKGL;
+        private readonly TransYTJ transYTJ;
+        public SingleTrans(Socket socket)
+        {
+            this.msgSendReceiver = new MsgSendReceiver(socket);
+            this.transDGL = new TransDGL(msgSendReceiver);
+            this.transDKJ = new TransDKJ(msgSendReceiver);
+            this.transKGL = new TransKGL(msgSendReceiver);
+            this.transYTJ = new TransYTJ(msgSendReceiver);
+        }
+        public TransDGL GetTransDGL()
+        {
+            return transDGL;
+        }
+        public TransDKJ GetTransDKJ()
+        {
+            return transDKJ;
+        }
+        public TransKGL GetTransKGL()
+        {
+            return transKGL;
+        }
+        public TransYTJ GetTransYTJ()
+        {
+            return transYTJ;
+        }
+        public MsgSendReceiver GetMSR()
+        {
+            return msgSendReceiver;
+        }
+    }
+}
