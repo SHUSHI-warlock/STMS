743784c757aa6504d6f016c83ef4cb406f1c21f0
张睿 <43677361+ZRE-ray@users.noreply.github.com>
2021-03-05 20:02:18 +0800

    判断店主卡是否存在

--- STMS-Server/src/main/java/DB/Dao.java
+++ STMS-Server/src/main/java/DB/Dao.java
@@ -915,10 +915,12 @@ public class Dao {
         try {
             state = conn.createStatement();
             rs = state.executeQuery(cal);
-            while (rs.next()) {
+            if (rs.next()) {
                 total -= rs.getInt("S_Rent");
                 master = rs.getString("S_Master");
             }
+            else
+                return -1;
         } catch (Exception e) {
             e.printStackTrace();
             return -1;
@@ -929,9 +931,11 @@ public class Dao {
         try {
             state = conn.createStatement();
             rs = state.executeQuery(fin);
-            while (rs.next()) {
+            if(rs.next()) {
                 total += rs.getInt("L_Lass");
             }
+            else
+                return -1;
         } catch (Exception e) {
             e.printStackTrace();
             return -1;
